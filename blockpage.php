bca3dde7a1601d5165550d3cbe7ddc32ac9b2d95
David <0x46697265@gmail.com>
2020-03-31 19:35:52 -0600

    Add files via upload

---
+++ blockpage.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>This site has been seized</title>
+		<style>
+			*, *::before, *::after {
+				box-sizing: border-box
+			}
+
+			body {
+				margin: 0;
+				width: 100%;
+				height: 100%;
+				background-color: #AE1635;
+				font-family: "Arial", sans-serif;
+				background-image: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='100px' width='105px'><text transform='translate(20, 100) rotate(-45)' fill='rgb(245,119,45)' font-size='37' style='font-family: Arial;font-weight:700'>Seized</text></svg>");
+			}
+
+			main {
+				width: 95%;
+				max-width: 900px;
+				padding: 50px;
+				min-height: 300px;
+				margin: 50px auto;
+				background-color: white;
+				-webkit-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.79);
+				-moz-box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.79);
+				box-shadow: 0px 0px 15px 0px rgba(0,0,0,0.79);
+			}
+
+			.seals {
+				text-align: center
+			}
+
+			.seal {
+				padding: 5px;
+				text-align: center;
+				display: inline-block;
+			}
+
+			.seal img {
+				height: 200px;
+				display:inline-block
+			}
+
+			p {
+				display: inline-block;
+				font-weight: 700;
+				font-size: 21px;
+				margin-top: 25px
+			}
+
+			.shadowed {
+				filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444')";
+				filter: url(#drop-shadow);
+				-webkit-filter: drop-shadow(0px 0px 7px rgba(0,0,0,0.79));
+				filter: drop-shadow(0px 0px 7px rgba(0,0,0,0.79));
+
+			}
+		</style>
+	</head>
+	<body>
+		<main>
+			<div class="seals">
+				<div class="seal">
+					<img src="doj.png" class="shadowed">
+				</div>
+				<div class="seal">
+					<img src="ipr.png" class="shadowed">
+				</div>
+				<div class="seal">
+					<img src="dohs.png" class="shadowed">
+				</div>
+			</div>
+			<p>This domain name has been sized by ICE - Homeland Security Investigations, pursuant to a seizure warrant issed by a United States District Court under the authority of 18 U.S.C 981 &sect;&sect; and 2323.</p>
+			<p><i>
+				Willful copyright infringement is a federal crime that carries penalties for first time offenders of up to five years in federal prison, a $250,000 fine, forfieture and restitution (17 U.S.C &sect; 506, 18 U.S.C &sect; 2319). Intentionally and knowingly trafficing counterfeit goods is a federal crime that carries penalties for first time offenders of up to ten years in federal prison, a $2,000,000 fine, forfeiture and restitution (18 U.S.C. &sect; 2320).
+			</i></p>
+		</main>
+	</body>
+</html>
